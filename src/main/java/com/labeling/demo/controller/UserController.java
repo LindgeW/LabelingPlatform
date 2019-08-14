01b93cde3afe64f4a95175edf6c4df387b5a01ee
张翔 <309784823@qq.com>
2019-08-14 21:26:41 +0800

    增加管理员界面可视化图表

--- src/main/java/com/labeling/demo/controller/UserController.java
+++ src/main/java/com/labeling/demo/controller/UserController.java
@@ -9,7 +9,6 @@ import com.labeling.demo.service.TeamService;
 import com.labeling.demo.service.UserService;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -39,15 +38,15 @@ public class UserController {
 
     @GetMapping("/bg_index")
     @RequiresRoles("admin")
-    public  String bgIndex(Model model){
+    public String bgIndex(Model model) {
         // 哪个任务，哪个团队中的哪个成员的工作进展
         List<User> allUsers = userService.findAll();
 
         List<TempoVO> tempos = new ArrayList<>();  //保存当前正在进行的用户进度信息
-        for (User user: allUsers) {
+        for (User user : allUsers) {
             String username = user.getUsername();
             String teamName = user.getTeamName();
-            if (StringUtils.isNotBlank(teamName)){
+            if (StringUtils.isNotBlank(teamName)) {
                 Team team = teamService.findByName(teamName);
                 String taskName = team.getTaskName();
                 Task task = taskService.findByName(taskName);
@@ -62,18 +61,41 @@ public class UserController {
         return "bg_index";
     }
 
+    @PostMapping("/usersInfo")
+    @ResponseBody
+    @RequiresRoles("admin")
+    public List<TempoVO> usersInfo() {
+        List<User> allUsers = userService.findAll();
+
+        List<TempoVO> tempos = new ArrayList<>();  //保存当前正在进行的用户进度信息
+        for (User user : allUsers) {
+            String username = user.getUsername();
+            String teamName = user.getTeamName();
+            if (StringUtils.isNotBlank(teamName)) {
+                Team team = teamService.findByName(teamName);
+                String taskName = team.getTaskName();
+                Task task = taskService.findByName(taskName);
+                Integer tagNum = instanceUserService.countByUsername(username);
+                Integer corpusSize = task.getCorpussize();
+                tempos.add(new TempoVO(username, teamName, taskName, tagNum, corpusSize));
+            }
+        }
+        return tempos;
+    }
+
+
     @GetMapping("/bg")
     @RequiresRoles("admin")
 //    @RequiresPermissions("login:annotate:bg")
-    public String background(Model model){
+    public String background(Model model) {
         User curUser = (User) SecurityUtils.getSubject().getPrincipal();
         model.addAttribute("username", curUser.getUsername());
         return "bg";
     }
 
     @GetMapping("/account")
-    public String account(Model model){
-        User user = (User)SecurityUtils.getSubject().getPrincipal();
+    public String account(Model model) {
+        User user = (User) SecurityUtils.getSubject().getPrincipal();
         model.addAttribute("userVo", new UserVO(user.getUsername(), user.getRole()));
         return "account";
     }
@@ -81,13 +103,13 @@ public class UserController {
 
     @PostMapping("/register")
     @ResponseBody
-    public RespEntity addUser(User user){
+    public RespEntity addUser(User user) {
         System.out.println(user);
         //账户信息保存到数据库中
         boolean isOk = this.userService.addUser(user);
-        if(isOk){
+        if (isOk) {
             return new RespEntity(RespStatus.SUCCESS);
-        }else{
+        } else {
             return new RespEntity(RespStatus.Error);
         }
     }
@@ -95,15 +117,15 @@ public class UserController {
     @PostMapping("/alter_pwd")
     @ResponseBody
     public RespEntity alterPwd(@RequestParam("oldPwd") String oldPwd,
-                               @RequestParam("newPwd") String newPwd){
+                               @RequestParam("newPwd") String newPwd) {
         User curUser = (User) SecurityUtils.getSubject().getPrincipal();
         String username = curUser.getUsername();
         String password = curUser.getPassword();
 //        User real = userService.findUser(curUser.getUsername());
         System.out.println(curUser);
-        if(!oldPwd.equals(password)){
+        if (!oldPwd.equals(password)) {
             return new RespEntity(RespStatus.UNAUTHEN);
-        } else if(!oldPwd.equals(newPwd)){
+        } else if (!oldPwd.equals(newPwd)) {
             User user = new User();
             user.setUsername(username);
             user.setPassword(newPwd);
